1fe37a63801ef0376f163b1ce902d7b8bde1f2bd
sunchang <sunchang612@gmail.com>
2017-08-07 22:57:48 +0800

    [UPDATE] 调用歌单接口

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -19,7 +19,7 @@
 </template>
 <script>
 import Slider from 'base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 export default {
   data () {
@@ -29,12 +29,20 @@ export default {
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
         }
       })
     }
