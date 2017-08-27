4b259480493d3e7ebc0f3d6042fe0b543dddde7d
sunchang <sunchang612@gmail.com>
2017-08-27 22:43:43 +0800

    [ADD,UPDATE] 添加获取歌手数据方法/ 通过接口获取数据

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import fastclick from 'fastclick'
 import '@/common/stylus/index.styl'
 import VueLazyLoad from 'vue-lazyload'
 
-fastclick.attach(document.body) // 结觉移动端事件的三百毫秒的延迟
+fastclick.attach(document.body) // 解决移动端事件的三百毫秒的延迟
 /* eslint-disable no-new */
 Vue.use(VueLazyLoad, {  // 使用懒加载插件
   loading: require('common/image/loading.gif')
