d9ce8da0240da0665706e6e55c2794e2e710ee5c
sunchang612 <sunchang612@gmail.com>
2017-09-17 22:34:14 +0800

    update

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="singer-detail"></div>
+  <transition name="slide">
+    <div class="singer-detail">1111111111</div>
+  </transition>
 </template>
 <script>
 export default {
@@ -16,4 +18,9 @@ export default {
   right :0
   bottom: 0
   background : $color-background
+.slide-enter-active, .slide-leave-active
+    transition: all 0.3s
+
+.slide-enter, .slide-leave-to
+    transform: translate3d(100%, 0, 0)
 </style>
