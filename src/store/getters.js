f5f1d8940004f64fd85038ff7921b70c3238ff55
sunchang <sunchang612@gmail.com>
2018-11-08 23:33:57 +0800

    :wrench: 设计 Vuex 数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,2 +1,14 @@
 // 取数据
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+export const fullScreen = state => state.fullScreen
+export const playlist = state => state.playlist
+export const sequenceList = state => state.sequenceList
+export const mode = state => state.node
+export const currentIndex = state => state.currentIndex
+
+// getters 也可以充当计算属性
+export const currentSong = (state) => {
+  return state.playing[state.currentIndex] || {}
+}
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,6 +4,32 @@ import * as types from './mutations-type'
 const mutations = {
   [types.SET_SINGER](state, singer) {
     state.singer = singer
+  },
+
+  [types.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag
+  },
+
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+
+  [types.SET_PLAY_LIST](state, list) {
+    state.playing = list
+  },
+
+  [types.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+
+  [types.SET_PLAY_MODE](state, mode) {
+    state.mode = mode
+  },
+
+  [types.SET_CURRENT_INDEX](state, index) {
+    state.currentIndex = index
   }
 }
 export default mutations
+
+// 操作 state 必须要通过 mutations 来修改
